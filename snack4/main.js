8d24b06f6cb3fc394a014580be463e4f2835bea2
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-12 17:28:42 +0100

    Completed

--- snack4/main.js
+++ snack4/main.js
@@ -1,1 +1,11 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let numero = +prompt("inserisci un numero")
+
+for(let i=0; i != 1; i++){
+    if((numero % 2) == 0){
+        console.log(numero)
+    }else{
+        console.log(numero+1)
+    }
+}
